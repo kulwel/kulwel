36a29a4507084855b567cdd872b5e1a01a09cfcd
lunabunn <iamrabbitmoon@gmail.com>
2025-03-16 04:04:04 -0400

    Bundle Pandoc for CF

--- _plugins/pandoc.rb
+++ _plugins/pandoc.rb
@@ -2,8 +2,13 @@ class Jekyll::Converters::Markdown::Pandoc
     def initialize(config)
         require "pandoc-ruby"
     end
-  
+
     def convert(content)
+        if ENV["CF_PAGES"]
+            pandoc_bin = File.expand_path("./pandoc")
+            PandocRuby.pandoc_path = pandoc_bin
+            puts "Using bundled Pandoc binary: #{pandoc_bin}"
+        end
         PandocRuby.new(content, from: "markdown+mark-subscript+hard_line_breaks+lists_without_preceding_blankline").to_html
     end
-  end
+end
